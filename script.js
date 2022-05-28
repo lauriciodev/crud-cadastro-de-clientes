dafa48567a3be00d399ca956e497761796304319
lauricio <lauriciomalek@gmail.com>
2022-05-28 16:24:20 -0300

    dfg

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const changeModal = () =>{
     Modal.classList.toggle("modal__open");
 }
 
-let clienteTeste = {
-nome:"bichona",
-email:"bicha@gmail.com",
-telefone:"09452792872",
-cidade:"sao paulo"
-};
+// campos inputs 
+let nome = document.getElementById("nome");
+let email = document.getElementById("email");
+let telefone = document.getElementById("telefone");
+let cidade = document.getElementById("cidade");
+
+
 
 
 const getLS = () => JSON.parse(localStorage.getItem ("dbCliente")) ?? [];
@@ -24,6 +25,38 @@ const createCliente = (cliente) =>{
 let dbcliente = readClient();
 dbcliente.push(cliente);
 setLS(dbcliente);
+
+}
+
+
+
+
+
+// função qur irá validar o cliente 
+const validCliente = () =>{
+    if(nome.value !== "" && email.value !== "" && telefone.value !== "" && cidade.value !== ""){
+        let newCliente = {
+            nome:nome.value,
+            email:email.value,
+           telefone:telefone.value,
+            cidade:cidade.value
+        }
+
+        createCliente(newCliente);
+        riseCliente();
+        changeModal();
+
+        nome.value = ""
+        email.value = ""
+        telefone.value = ""
+        cidade.value = ""
+
+
+    }else{
+        alert("preencha todos os campos!")
+    }
+
+
 }
 
 // função que ira atualizar os dado do  cliente 
@@ -46,15 +79,33 @@ const deleteCliente = (indice) =>{
    let dbCliente = readClient();
    dbCliente.splice(indice,1);
    setLS(dbCliente);
+   riseCliente()
    
 }
+
+
+// função que irá pegar o click 
+
+const getClick = (evento) =>{
+let click = evento.target
+let ind = click.dataset.indice
+let cliente = getLS()
+if(click.id === "del"){
+    if(confirm("deseja realmente deletar o cliente " + cliente[ind].nome )){
+    deleteCliente(ind)
+    }
+}
+
+}
+
+
 
 let contClientes = document.querySelector("#cont__clientes");
 
 
 // função que manda os dado para o front-end 
 
-const sendClient = (cliente) => {
+const sendClient = (cliente,indice) => {
     let newCliente = document.createElement("tr");
     newCliente.innerHTML = `
     <td>${cliente.nome}</td>
@@ -62,16 +113,17 @@ const sendClient = (cliente) => {
     <td>${cliente.telefone}</td>
     <td>${cliente.cidade}</td>
     <td>
-        <button class="btn"><i class="ri-delete-bin-7-fill"></i></button>
-        <button class="btn"><i class="ri-pencil-fill"></i></button>
+        <button class="btn" id="del" data-indice=${indice}><i class="ri-delete-bin-7-fill"></i></button>
+        <button class="btn" id="edit" data-indice=${indice}><i class="ri-pencil-fill"></i></button>
     </td>
     `
    contClientes.append(newCliente)
 }
 
 const riseCliente = () =>{
+    contClientes.innerHTML = "";
     let banco = readClient()
-   banco.forEach(sendClient)
+   banco.forEach((cliente,indice) => sendClient(cliente,indice))
     
 }
 riseCliente()
@@ -81,5 +133,7 @@ riseCliente()
 
 // eventos 
 
+contClientes.addEventListener("click",getClick)
+document.getElementById("btn__cadastro").addEventListener("click",validCliente)
 cancel.addEventListener("click",changeModal);
 callModal.addEventListener("click",changeModal);
